cf21ac0a674ec57c68dc14f0ad86272a38395219
AurelienWAXIN <aurelien.waxin@hotmail.fr>
2021-08-02 14:15:06 +0200

    Initial Commit

---
+++ INSTAGRID1.0.1/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  INSTAGRID1.0.1
+//
+//  Created by Aurélien Waxin on 27/07/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+ 
+    
+}
+
+//extension UIView {
+//
+//    // Using a function since `var image` might conflict with an existing variable
+//    // (like on `UIImageView`)
+//    func asImage() -> UIImage {
+//        let renderer = UIGraphicsImageRenderer(bounds: bounds)
+//        return renderer.image { rendererContext in
+//            layer.render(in: rendererContext.cgContext)
+//        }
+//    }
+//}
+
+
+
