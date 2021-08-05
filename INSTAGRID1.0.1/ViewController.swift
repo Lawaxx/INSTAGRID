ab16ae3fb30a8aeab02794a9cc91abd18a1f061e
AurelienWAXIN <aurelien.waxin@hotmail.fr>
2021-08-05 15:11:41 +0200

    update

--- INSTAGRID1.0.1/ViewController.swift
+++ INSTAGRID1.0.1/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     
     var photoButton : UIButton?
     
+    var layoutIsEmpty = true // This Boolean will permit to check wether the grid is empty or not before sharing
     let imageSelect = UIImage(named: "Selected")
     let layout1 = UIImage(named: "Layout 1")
     let layout2 = UIImage(named: "Layout 2")
@@ -33,11 +34,52 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        imagePicker.delegate = self
         // Do any additional setup after loading the view.
     }
     @IBAction func photoTapLoad(_ sender: UIButton) {
         print("Button Click")
+        self.photoButton = sender
+        imagePicker.allowsEditing = true
+                imagePicker.sourceType = .photoLibrary
+                imagePicker.mediaTypes = UIImagePickerController.availableMediaTypes(for: .photoLibrary)!
+                present(imagePicker, animated: true, completion: nil)
         }
+    
+    @IBAction func photoTapLoad2(_ sender: UIButton) {
+        print("Button Click")
+        self.photoButton = sender
+        imagePicker.allowsEditing = true
+                imagePicker.sourceType = .photoLibrary
+                imagePicker.mediaTypes = UIImagePickerController.availableMediaTypes(for: .photoLibrary)!
+                present(imagePicker, animated: true, completion: nil)
+        }
+    
+    
+    
+    
+    @IBAction func photoTapLoad3(_ sender: UIButton) {
+        print("Button Click")
+        self.photoButton = sender
+        imagePicker.allowsEditing = true
+                imagePicker.sourceType = .photoLibrary
+                imagePicker.mediaTypes = UIImagePickerController.availableMediaTypes(for: .photoLibrary)!
+                present(imagePicker, animated: true, completion: nil)
+        }
+    
+    
+    
+    @IBAction func photoTapLoad4(_ sender: UIButton) {
+        print("Button Click")
+        self.photoButton = sender
+        imagePicker.allowsEditing = true
+                imagePicker.sourceType = .photoLibrary
+                imagePicker.mediaTypes = UIImagePickerController.availableMediaTypes(for: .photoLibrary)!
+                present(imagePicker, animated: true, completion: nil)
+        }
+    
+    
+    
     
     @IBAction func layoutButton3(_ sender: UIButton) {
         print("Layout Button 3 Click")
@@ -72,19 +114,101 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     @IBOutlet weak var lowRightButton: UIButton!
    
     
-    
-//     This method will launch the imagePicker when a button is tapped
-//        @IBAction func loadImageButtonTapped(_ sender: UIButton) {
-//            self.photoButton = sender
-//            imagePicker.allowsEditing = true
-//            imagePicker.sourceType = .photoLibrary
-//            imagePicker.mediaTypes = UIImagePickerController.availableMediaTypes(for: .photoLibrary)!
-//            present(imagePicker, animated: true, completion: nil)
+//    @IBAction func handleSwipes(_ sender: UISwipeGestureRecognizer?) {
+//        if let gesture = sender {
+//            if UIDevice.current.orientation.isPortrait && gesture.direction == .up {
+//                moveBlueViewUp()
+//            } else if UIDevice.current.orientation.isLandscape && gesture.direction == .left {
+//                moveMainViewLeft()
+//            }
 //        }
+//    }
+//func moveBlueViewUp() {
+//             UIView.animate(withDuration: 2, animations: {
+//                 self.blueView.transform = CGAffineTransform(translationX: 0, y: -UIScreen.main.bounds.height)
+//             }, completion: {
+//                 (true) in
+//                 self.checkLayout()
+//             })
+//         }
+//         func moveMainViewLeft() {
+//             UIView.animate(withDuration: 2, animations: {
+//                 self.blueView.transform = CGAffineTransform(translationX: -UIScreen.main.bounds.width, y: 0)
+//             }, completion: {
+//                 (true) in
+//                 self.checkLayout()
+//             })
+//         }
+//
+////    @IBAction func handleSwipe(_ sender: UISwipeGestureRecognizer?) {
+////            if let gesture = sender {
+////                if UIDevice.current.orientation.isPortrait && gesture.direction == .up {
+////                    moveBlueViewUp()
+////                } else if UIDevice.current.orientation.isLandscape && gesture.direction == .left {
+////                    moveMainViewLeft()
+////                }
+////            }
+////        }
+////    func moveBlueViewUp() {
+////                 UIView.animate(withDuration: 2, animations: {
+////                     self.blueView.transform = CGAffineTransform(translationX: 0, y: -UIScreen.main.bounds.height)
+////                 }, completion: {
+////                     (true) in
+////                     self.checkLayout()
+////                 })
+////             }
+////             func moveMainViewLeft() {
+////                 UIView.animate(withDuration: 2, animations: {
+////                     self.blueView.transform = CGAffineTransform(translationX: -UIScreen.main.bounds.width, y: 0)
+////                 }, completion: {
+////                     (true) in
+////                     self.checkLayout()
+////                 })
+////             }
+//
+//    func shareLayout() {
+//              let content = blueView.asImage()
+//              let activityController = UIActivityViewController(activityItems: [content], applicationActivities: nil)
+//              self.present(activityController, animated: true, completion: nil)
+//              // We use the completion handler to move back the mainView when the activityController is closed
+//              activityController.completionWithItemsHandler = {  (activity, success, items, error) in
+//                  UIView.animate(withDuration: 1, animations: {
+//                      self.blueView.transform = .identity
+//                  }, completion: nil)
+//              }
+//          }
+//
+//    // This function will present an alert if the user tries to share an empty grid
+//          func checkLayout() {
+//              let alert = UIAlertController(title: "Empty Grid", message: "Are you sure you want to share an empty grid ?", preferredStyle: .alert)
+//              alert.addAction(UIAlertAction(title: "Yes", style: .default, handler: { action in
+//                  self.shareLayout() } ))
+//              alert.addAction(UIAlertAction(title: "No", style: .cancel, handler: { action in
+//                  self.blueView.transform = .identity
+//              }))
+//              if layoutIsEmpty == true {
+//                  self.present(alert, animated: true)
+//              } else {
+//                  shareLayout()
+//              }
+//          }
 
 
-
-
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
             if let pickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
                 self.photoButton?.setImage(pickedImage, for: UIControl.State.normal)
@@ -92,12 +216,18 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             }
             dismiss(animated: true, completion: nil)
         }
+    
+    
 
         // METHOD FOR THE DELEGATE: when the user cancels image picking
         func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
             dismiss(animated: true, completion: nil)
         }
 }
+
+
+
+
     // This UIView extension will permit to convert our MainView to an image file
     extension UIView {
         func asImage() -> UIImage {
